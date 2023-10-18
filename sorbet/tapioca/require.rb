d39f14e7d1e1b1b4270917262b23ef5a2cf15566
Andy Waite <andyw8@users.noreply.github.com>
2023-10-18 07:57:17 -0400

    sorbet/tapioca/require.rb

--- sorbet/tapioca/require.rb
+++ sorbet/tapioca/require.rb
@@ -3,18 +3,5 @@
 
 # Add your extra requires here (`bin/tapioca require` can be used to bootstrap this list)
 
-require_relative '../../app/controllers/application_controller'
-
-devise_path = Gem.loaded_specs['devise'].full_gem_path
-$LOAD_PATH << "#{devise_path}/app/controllers"
-$LOAD_PATH << "#{devise_path}/app/helpers"
-$LOAD_PATH << "#{devise_path}/app/mailers"
-
-require 'devise_helper'
-require 'devise_controller'
-require 'devise/confirmations_controller'
-require 'devise/sessions_controller'
-require 'devise/registrations_controller'
-require 'devise/unlocks_controller'
-require 'devise/passwords_controller'
-require 'devise/omniauth_callbacks_controller'
+# require_relative '../../app/controllers/application_controller'
+$LOAD_PATH.concat(Devise::Engine.config.eager_load_paths)
